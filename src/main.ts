d7b6f42bcd76ecd0c96871d8e3b4ff483816f4aa
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-07-29 12:30:34 +0200

    feat: enable global JwtAuthGuard in main.ts
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-/*
-import * as fs from 'fs/promises';
-import * as path from 'path';
-import { QuotesService } from './quote/quote.service';
-import { Quote } from './entity/quote.entity';
-*/
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Applies ValidationPipe globally
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -19,22 +13,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  /*
-  const quotesService = app.get(QuotesService);
-  console.log('✅ Bootstrapping...');
-  // Seed only if DB is empty
-  const count = (await quotesService.findAll()).length;
-  if (count === 0) {
-    const dataPath = path.join(__dirname, './quote/quotes.json');
-    const raw = await fs.readFile(dataPath, 'utf-8');
-    const quotes = JSON.parse(raw) as Quote[];
-    for (const quote of quotes) {
-      await quotesService.create(quote);
-    }
-    console.log('✅ Dummy data seeded.');
-  }
-  */
+
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   await app.listen(3006);
 }
-bootstrap();
+void bootstrap();
